c59518fb66c0035fe124c79cb85bc6df69ebcf4d
Adrián Gil Reyes <gilreeyes.5@gmail.com>
2026-03-10 13:42:00 +0100

    Primer commit: Traductor IA

---
+++ data/src/models/Transformer.py
@@ -0,0 +1,19 @@
+from transformers import pipeline
+from langdetect import detect
+
+español_ingles = pipeline("any-to-any",model='Helsinki-NLP/opus-mt-es-en') #ESPAÑOL - INGLÉS
+ingles_español = pipeline("any-to-any",model='Helsinki-Nlp/opus-mt-en-es') # INGLÉS - ESPAÑOL
+
+while True:
+    text_translate = input('Introduce a text:') 
+
+    if text_translate.lower() == 'exit':
+        break 
+
+    idioma = detect(text_translate)
+
+    if idioma == "es":
+        traduccion = español_ingles(text_translate)[0]('generated_text')
+    
+    elif idioma == "en":
+        traduccion = ingles_español(text_translate)[0]('generated_text') 
